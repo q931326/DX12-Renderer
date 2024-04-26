5452df8410580397193400c13ed906eb7baec72e
Qinzefan <124950885+q931326@users.noreply.github.com>
2024-04-26 21:42:22 +0800

    Update Default.hlsl

--- Shaders/Default.hlsl
+++ Shaders/Default.hlsl
@@ -43,19 +43,19 @@ VertexOut VS(VertexIn vin)
     float4 posW = mul(float4(vin.PosL, 1.0f), gWorld);
     vout.PosW = posW.xyz;
 
-    // Assumes nonuniform scaling; otherwise, need to use inverse-transpose of world matrix.
+    // 假设缩放不均匀;否则，需要使用 World Matrix 的反向转置。
     vout.NormalW = mul(vin.NormalL, (float3x3)gWorld);
 	
 	vout.TangentW = mul(vin.TangentU, (float3x3)gWorld);
 
-    // Transform to homogeneous clip space.
+    // 转换为均匀的裁剪空间。
     vout.PosH = mul(posW, gViewProj);
 	
-	// Output vertex attributes for interpolation across triangle.
+	// 输出用于跨三角形插值的顶点属性。
 	float4 texC = mul(float4(vin.TexC, 0.0f, 1.0f), gTexTransform);
 	vout.TexC = mul(texC, matData.MatTransform).xy;
 
-    // Generate projective tex-coords to project shadow map onto scene.
+    // 生成投影 tex-coords 以将阴影贴图投影到场景中。
     vout.ShadowPosH = mul(posW, gShadowTransform);
 	
     return vout;
@@ -63,7 +63,7 @@ VertexOut VS(VertexIn vin)
 
 float4 PS(VertexOut pin) : SV_Target
 {
-	// Fetch the material data.
+	// 获取材料数据。
 	MaterialData matData = gMaterialData[gMaterialIndex];
 	float4 diffuseAlbedo = matData.DiffuseAlbedo;
 	float3 fresnelR0 = matData.FresnelR0;
@@ -71,23 +71,20 @@ float4 PS(VertexOut pin) : SV_Target
 	uint diffuseMapIndex = matData.DiffuseMapIndex;
 	uint normalMapIndex = matData.NormalMapIndex;
 	
-    // Dynamically look up the texture in the array.
+    // 动态查找数组中的纹理。
     diffuseAlbedo *= gTextureMaps[diffuseMapIndex].Sample(gsamAnisotropicWrap, pin.TexC);
 
 #ifdef ALPHA_TEST
-    // Discard pixel if texture alpha < 0.1.  We do this test as soon 
-    // as possible in the shader so that we can potentially exit the
-    // shader early, thereby skipping the rest of the shader code.
+    // 如果纹理 alpha < 0.1，则丢弃像素。 我们会尽快进行此测试，尽可能在着色器中，以便我们可以潜在地退出着色器，从而跳过着色器代码的其余部分。
     clip(diffuseAlbedo.a - 0.1f);
 #endif
 
-	// Interpolating normal can unnormalize it, so renormalize it.
+	// 插值法线可以取消规范化它，因此请重新规范化它。
     pin.NormalW = normalize(pin.NormalW);
 	
 	float4 normalMapSample = gTextureMaps[normalMapIndex].Sample(gsamAnisotropicWrap, pin.TexC);
 	float3 bumpedNormalW = NormalSampleToWorldSpace(normalMapSample.rgb, pin.NormalW, pin.TangentW);
 
-	// Uncomment to turn off normal mapping.
     //bumpedNormalW = pin.NormalW;
 
     // Vector from point being lit to eye. 
