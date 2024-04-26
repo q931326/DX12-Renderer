2e9d720a7be49eb67a8b224aeefecb9724d6eda9
Qinzefan <124950885+q931326@users.noreply.github.com>
2024-04-26 21:44:27 +0800

    Update Sky.hlsl

--- Shaders/Sky.hlsl
+++ Shaders/Sky.hlsl
@@ -21,13 +21,11 @@ VertexOut VS(VertexIn vin)
 	// Use local vertex position as cubemap lookup vector.
 	vout.PosL = vin.PosL;
 	
-	// Transform to world space.
 	float4 posW = mul(float4(vin.PosL, 1.0f), gWorld);
 
 	// Always center sky about camera.
 	posW.xyz += gEyePosW;
 
-	// Set z = w so that z/w = 1 (i.e., skydome always on far plane).
 	vout.PosH = mul(posW, gViewProj).xyww;
 	
 	return vout;
